04810f4e2d7d6c57e9a4965eb1c07579e34400e1
DHANUSRI1608 <dhanusriii.1608@gmail.com>
2025-09-24 10:49:58 +0530

    PowersOf2

---
+++ Arrays/PowersOfTwo/answer.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class answer {
+    public static void main(String args[])
+    {
+           Scanner sc = new Scanner(System.in);
+           int n = sc.nextInt();
+           if(n > 0 && (n & (n-1))== 0)
+           {
+            System.out.print("POWER OF TWO");
+           }
+           else{
+            System.out.print("NOT A POWER OF TWO");
+           }
+    }
+}
