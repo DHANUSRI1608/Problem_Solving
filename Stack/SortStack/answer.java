95f7c038d8f86977743c0b6d5bb58aabe5f415fb
DHANUSRI1608 <dhanusriii.1608@gmail.com>
2025-09-25 22:44:28 +0530

    Stack

---
+++ Stack/SortStack/answer.java
@@ -0,0 +1,41 @@
+package Stack.SortStack;
+import java.util.*;
+
+public class answer {
+    public static void recursion(Stack<Integer> stc, int x) {
+        if (stc.isEmpty() || stc.peek() <= x) {
+            stc.push(x);
+            return;
+        }
+        int top = stc.pop();
+        recursion(stc, x);
+        stc.push(top);
+    }
+
+    public static void sort(Stack<Integer> stc) {
+        if (stc.isEmpty()) return;
+        int top = stc.pop();
+        sort(stc);
+        recursion(stc, top);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        Stack<Integer> stc = new Stack<>();
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            int val = sc.nextInt();
+            stc.push(val); 
+        }
+
+        sort(stc);  // now works without object
+
+        System.out.println("Sorted stack (top to bottom):");
+        while (!stc.isEmpty()) {
+            System.out.print(stc.pop() + " ");
+        }
+    }
+}
