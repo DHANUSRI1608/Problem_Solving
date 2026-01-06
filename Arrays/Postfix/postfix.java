fbc98aa6e55f999e924c17bba57494281e3e4b52
DHANUSRI1608 <dhanusriii.1608@gmail.com>
2026-01-06 21:56:12 +0530

    Postfix

---
+++ Arrays/Postfix/postfix.java
@@ -0,0 +1,36 @@
+package Arrays.Postfix;
+
+import java.util.*;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            String token = tokens[i];
+
+            if (token.equals("+") || token.equals("-") ||
+                token.equals("*") || token.equals("/")) {
+
+                int b = stack.pop();   
+                int a = stack.pop();   
+                int ans = 0;
+
+                if (token.equals("+")) {
+                    ans = a + b;
+                } else if (token.equals("-")) {
+                    ans = a - b;
+                } else if (token.equals("*")) {
+                    ans = a * b;
+                } else {
+                    ans = a / b;
+                }
+
+                stack.push(ans);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+     
+    }
+}
